17c430b013ca2482a91402c7a7151b5199048204
gweseloh <greg.weseloh@gmail.com>
2025-03-12 14:52:29 -0500

    Printed movies titles from an array

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
+let movies = [
+    ["Anora", "The Brutalist", "A Complete Unknown"],
+    ["Conclave", "Dune: Part Two", "I'm Still Here"],
+    ["Nickel Boys", "The Substance", "Wicked"]
+];
 
+console.log(movies[0][0], movies[0][1], movies[0][2], movies[1][0], movies[1][1], movies[1][2], movies[2][0], movies[2][1], movies[2][2], "\n");
+
+for (let row = 0; row <= 2; row++) {
+    for (let item = 0; item <= 2; item++) {
+        console.log(movies[row][item]);
+    };
+};
+console.log("\n")
+
+let shelf = 1;
+for (i = 0; i <=2; i++) {
+    console.log(movies[shelf][i]);
+};
